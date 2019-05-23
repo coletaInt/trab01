c1c6f279d81b5e91f74075b7702d25b2b4aee856
Kazushi Sakurada <kazushisakurada@icloud.com>
2019-05-23 09:31:33 -0300

    fix: Update sql code based by model print

--- arquivos/TrashMeBD.sql
+++ arquivos/TrashMeBD.sql
@@ -1,12 +1,24 @@
-create table usuario (id_usuario serial, email varchar(50),senha varchar(20),nome_user varchar(50),id_permissao int);
-create table permissao (id_permissao serial, tipo_permissao varchar(30));
-create table lixeira (id_lixeira serial, nome_lixo varchar(20), latitude float,longitude float, id_nv_lixo int, id_situacao int);
-create table nv_lixo (id_nv_lixo serial, tipo_nv_lixo varchar(50));
-create table situacao (id_situacao serial, tipo_situacao varchar(20));
-create table manutencao (id_manutencao serial, id_lixeira int, data date, hora time, id_usuario int, descricao varchar(50));
+create table usuario (id_usuario int, email varchar(50),senha varchar(20),nome_user varchar(50),id_permissao int, unique(id_usuario));
+create table permissao (id_permissao int, tipo_permissao varchar(30), unique(id_permissao));
+create table manutencao (id_manutencao int, id_lixeira int, data date, hora time, descricao varchar(50), unique(id_manutencao));
+create table lixeira (id_lixeira int, id_situacao int, id_nv_lixo int, nome_lixo varchar(20), latitude float,longitude float, unique(id_lixeira));
+create table nv_lixo (id_nv_lixo int, tipo_nv_lixo varchar(20), unique(id_nv_lixo));
+create table situacao (id_situacao int, tipo_situacao varchar(20), unique(id_situacao));
 
-insert into nv_lixo (tipo_nv_lixo) values ('vazio'),('meio_vazio'),('meio'),('meio_cheio'),('cheio');
-insert into permissao (tipo_permissao) values ('Funcionario'),('Morador');
-insert into usuario (email, senha, nome_user, id_permissao) values('pedrocruz@test.com', '123456', 'Pedro Cruz', 2), ('kazushi@test.com', '123456', 'Kazushi', 2), ('brian@test.com', '123456', 'Brian', 1), ('marcelo@test.com', '123456', 'Marcelo', 1);
-insert into lixeira (nome_lixo, latitude, longitude, id_nv_lixo) values('Quadra', 123.2312, -123.4232, 1), ('Area de Festas', 324.1230, -12.2123, 2), ('Piscina', 534.3214, 123.6045, 4);
-insert into situacao (tipo_situacao) values('operante'), ('inoperante');
+alter table usuario add primary key(id_usuario);
+alter table permissao add primary key(id_permissao);
+alter table lixeira add primary key(id_lixeira);
+alter table manutencao add primary key(id_manutencao);
+alter table nv_lixo add primary key(id_nv_lixo);
+alter table situacao add primary key(id_situacao);
+
+alter table usuario add foreign key(id_permissao) references permissao(id_permissao);
+alter table manutencao add foreign key(id_lixeira) references lixeira(id_lixeira);
+alter table lixeira add foreign key(id_nv_lixo) references nv_lixo(id_nv_lixo);
+alter table lixeira add foreign key(id_situacao) references situacao(id_situacao);
+
+insert into nv_lixo (id_nv_lixo, tipo_nv_lixo) values (0, 'vazio'),(1, 'meio_vazio'),(2, 'meio'),(3, 'meio_cheio'),(4, 'cheio');
+insert into permissao (id_permissao, tipo_permissao) values (0, 'Funcionario'),(1, 'Morador');
+insert into usuario (id_usuario, email, senha, nome_user, id_permissao) values(10,'pedrocruz@test.com', '123456', 'Pedro Cruz', 1), (11,'kazushi@test.com', '123456', 'Kazushi', 1), (12,'brian@test.com', '123456', 'Brian', 0), (13,'marcelo@test.com', '123456', 'Marcelo', 0);
+insert into situacao (id_situacao,tipo_situacao) values(1,'operante'), (0,'inoperante');
+insert into lixeira (id_lixeira, nome_lixo, latitude, longitude, id_nv_lixo, id_situacao) values(1,'Quadra', 123.2312, -123.4232, 1, 1), (2,'Area de Festas', 324.1230, -12.2123, 2, 1), (3,'Piscina', 534.3214, 123.6045, 4, 1);
